656f3b0b59b68406e6eecc88083bc8581a7cf023
ajstamm <astamm@albany.edu>
2022-07-21 14:11:10 -0400

    Update read_water_areas.R

--- R/read_water_areas.R
+++ R/read_water_areas.R
@@ -32,7 +32,7 @@ prep_water_areas_obsolete <- function(shp_path, gdb, shp_name, save_path = NULL)
   w <- sf::st_combine(w) # combine all geometries into one feature
   sf::sf_use_s2(FALSE) # because combining can invalidate areas,
                        # and make_valid won't work right -
-                       # FALSE turns off spherical gemetry check
+                       # FALSE turns off spherical geometry check
   w <- sf::st_union(w) # union all geometries (must combine first?)
   if (!is.null(save_path)) {
     sf::st_write(w, dsn = save_path, layer = "water_areas",
@@ -43,11 +43,11 @@ prep_water_areas_obsolete <- function(shp_path, gdb, shp_name, save_path = NULL)
 
 prep_water_areas <- function(shppath, shp, gdb) {
   # protected areas ----
-  # download your state from 
+  # download your state from
   # https://www.usgs.gov/core-science-systems/science-analytics-and-synthesis/gap/science/pad-us-data-download?qt-science_center_objects=0#qt-science_center_objects
   p <- sf::st_read(dsn = shppath, layer = shp)
   # p <- rgdal::readOGR(dsn = shppath, layer = shp)
-  # remove “Wild and Scenic River” (WSR), “Special Designation Area” (SDA), and 
+  # remove “Wild and Scenic River” (WSR), “Special Designation Area” (SDA), and
   #        “National Scenic, Botanical or Volcanic” (NSBV)
   p <- p[!p$Des_Tp %in% c("NSBV", "WSR", "SDA"), ]
   # validate polygons n=11214
@@ -56,12 +56,12 @@ prep_water_areas <- function(shppath, shp, gdb) {
   p <- sf::st_union(p)
   p <- sf::as_Spatial(p)
   # pa_d <- raster::aggregate(pa, dissolve = TRUE)
-  # above failed (timed out?), so try: 
+  # above failed (timed out?), so try:
   # pa_d <- rgeos::gUnaryUnion(pa)
-  
+
   # water bodies ----
   # read in water features
-  # download your state from 
+  # download your state from
   # https://apps.nationalmap.gov/downloader/#/
   # load NHDArea, NHDWaterbody
   # rgdal::ogrListLayers("data/NHD/NHD_H_New_York_State_GDB.gdb")
@@ -76,11 +76,11 @@ prep_water_areas <- function(shppath, shp, gdb) {
   w <- sf::st_union(w)
   w <- sf::st_zm(w)
   w <- sf::as_Spatial(w)
-  
+
   # merge water (nhdw) and protected areas (pa)
   d <- raster::bind(p, w)
   # pa_w2 <- raster::bind(pa_d, nhdw_d)
-  
+
   return(d)
 }
 
